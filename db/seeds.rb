1f15146eebabb85a9acd064b419851eb6f7300ec
carlos reyes <carlosreyes83@gmail.com>
2011-09-30 18:30:06 -0300

    agregue una lineas al archivo seeds.rb para cargar la base de datos con datos cuando se ejecuta el comando rake db:seed.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Nadador.create(:nombre => 'Carlos', :apellido => 'Reyes', :fecha_nacimiento => '1983/12/30', :club => 'Boca',:sexo => 'M')
+Nadador.create(:nombre => 'Juliana', :apellido => 'Treyes', :fecha_nacimiento => '1970/03/12', :club => 'Riber',:sexo => 'F')
+Nadador.create(:nombre => 'Jacinto', :apellido => 'Pereyra', :fecha_nacimiento => '1978/12/30', :club => 'Deportivo San Fernando',:sexo => 'M')
+Nadador.create(:nombre => 'Carolina', :apellido => 'Risso', :fecha_nacimiento => '1986/12/30', :club => 'Centro Gallego',:sexo => 'F')
+
